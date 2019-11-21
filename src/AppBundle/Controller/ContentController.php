04a9616d98502dd546c27544f5c0252fa1132f73
RemyVase <44263729+RemyVase@users.noreply.github.com>
2019-11-21 16:19:46 +0100

    Add files via upload

---
+++ src/AppBundle/Controller/ContentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContentController extends FrontendController
+{
+    public function defaultAction(Request $request)
+    {
+    }
+
+    public function productAction(Request $request)
+    {
+    }
+
+    public function welcomeAction(Request $request)
+    {	
+    }
+
+    public function equipeAction(Request $request)
+    {
+    }
+
+    public function proposAction(Request $request)
+    {
+    }
+
+    public function conseilsAction(Request $request)
+    {
+    }
+}
